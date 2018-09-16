a3c3b3a7c9033e0f92a154cf540d84c9123d5e83
aswinshenoy <aswinshenoy65@gmail.com>
2018-09-17 01:30:14 +0530

    Registered newly created models in admin

--- moviedb/movie_data/admin.py
+++ moviedb/movie_data/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import movie, artist, roles, artistAdmin, movieAdmin
 
+admin.site.register(movie, movieAdmin)
+admin.site.register(artist, artistAdmin)
+admin.site.register(roles)
 # Register your models here.
