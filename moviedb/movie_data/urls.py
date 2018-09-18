4ca6759d41f48e635ab643a942b3fd553cb64907
aswinshenoy <aswinshenoy65@gmail.com>
2018-09-18 06:54:47 +0530

    Added base template support, created views to list movies and show movie

--- moviedb/movie_data/urls.py
+++ moviedb/movie_data/urls.py
@@ -3,6 +3,6 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url('movies', TemplateView.as_view(template_name='movie/list.html')),
+    url('movies', views.movielist, name="movielist"),
     url(r'^movie/(?P<id>[0-9]+)/$', views.moviepage, name="moviepage"),
 ]
--- moviedb/movie_data/views.py
+++ moviedb/movie_data/views.py
@@ -4,3 +4,7 @@ from . import models
 def moviepage(request,id):
     movie = models.movie.objects.get(pk=id)
     return render(request, 'movie/single.html',{'movie':movie})
+
+def movielist(request):
+     movies = models.movie.objects.all()
+     return render(request, 'movie/list.html', {'movies': movies})
