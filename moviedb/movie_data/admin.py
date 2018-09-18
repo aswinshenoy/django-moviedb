f2c5b363328dfa04c5db7044479b864e65ca29df
aswinshenoy <aswinshenoy65@gmail.com>
2018-09-18 05:44:58 +0530

    removed unneccesary comment line on admin.py

--- moviedb/movie_data/admin.py
+++ moviedb/movie_data/admin.py
@@ -4,4 +4,3 @@ from .models import movie, artist, roles, artistAdmin, movieAdmin
 admin.site.register(movie, movieAdmin)
 admin.site.register(artist, artistAdmin)
 admin.site.register(roles)
-# Register your models here.
